6e4141af81c158bf358c9de8e5928529836b590c
Starford Omwakwe <starfordomwakwe@gmail.com>
2020-11-18 10:09:01 +0300

    add top nav add login component add routing btwn home and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './components/home/home.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TopNavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
